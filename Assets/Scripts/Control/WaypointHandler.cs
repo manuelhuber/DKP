b88435c815e31de1889407c18f3eb8546ae76bb9
Manuel Huber <nelo112@web.de>
2017-09-07 16:26:55 +0200

    fixed active waypoint line

--- Assets/Scripts/Control/WaypointHandler.cs
+++ Assets/Scripts/Control/WaypointHandler.cs
@@ -8,35 +8,76 @@ namespace Control {
         public GameObject ActiveWaypointMarkerPrefab;
         public Color WaypointLineColor;
         public float WaypointLineWidth;
+        public Animator Animator;
 
         private NavMeshAgent agent;
         private GameObject currentDestination;
         private LineRenderer currentDestinationLineRenderer;
         private readonly List<GameObject> waypoints = new List<GameObject>();
 
-        private void Start() {
-            agent = GetComponent<NavMeshAgent>();
+        public void GoDirectlyTo(ClickLocation location) {
+            ClearWaypoints();
+            AddWaypoint(location);
+            GoToNextWaypoint();
         }
-
 
         public void DestroyCurrentWaypoint() {
             Destroy(currentDestination);
         }
 
         /// <summary>
+        /// Adds a waypoint and renders a line to the previous wapoint
+        /// </summary>
+        public void AddWaypoint(ClickLocation clickLocation) {
+            GameObject marker;
+            var markerWrapper = CreateMarker(InactiveWaypointMarkerPrefab, clickLocation.Location, out marker);
+            waypoints.Add(markerWrapper);
+
+            // Connect waypoint to previous waypoint
+            var previousWaypoint = waypoints.Count < 2 ? currentDestination : waypoints[waypoints.Count - 2];
+            if (previousWaypoint == null) return;
+            var lineRenderer = marker.GetComponent<LineRenderer>();
+            lineRenderer.SetPosition(0, waypoints[waypoints.Count - 1].transform.position);
+            lineRenderer.SetPosition(1, previousWaypoint.transform.position);
+            currentDestinationLineRenderer.enabled = true;
+        }
+
+        private GameObject CreateMarker(GameObject prefab, Vector3 location, out GameObject marker) {
+            var markerWrapper = new GameObject("Waypoint");
+            markerWrapper.transform.position = location;
+            var markerLocation = location;
+            markerLocation.y += prefab.transform.localScale.y / 2;
+            marker = Instantiate(
+                prefab,
+                markerLocation,
+                prefab.transform.rotation
+            );
+            marker.transform.SetParent(markerWrapper.transform);
+            return markerWrapper;
+        }
+
+        public void ToggleWaypointRenderer(bool value) {
+            waypoints.ForEach(o => {
+                o.SetActive(value);
+                var line = o.GetComponentInChildren<LineRenderer>();
+                line.enabled = value;
+            });
+            if (currentDestinationLineRenderer == null) return;
+            currentDestinationLineRenderer.enabled = value;
+        }
+
+
+        /// <summary>
         /// Makes the next waypoint the current destination
         /// </summary>
-        public void GoToNextWaypoint() {
+        private void GoToNextWaypoint() {
             if (waypoints.Count <= 0) return;
             var next = waypoints[0];
             var nextPosition = next.transform.position;
             agent.SetDestination(nextPosition);
-            currentDestination = Instantiate(
-                ActiveWaypointMarkerPrefab,
-                nextPosition,
-                ActiveWaypointMarkerPrefab.transform.rotation);
-            currentDestinationLineRenderer = currentDestination.GetComponent<LineRenderer>();
-            currentDestinationLineRenderer.enabled = false;
+            GameObject foo;
+            currentDestination = CreateMarker(ActiveWaypointMarkerPrefab, nextPosition, out foo);
+            currentDestinationLineRenderer = foo.GetComponent<LineRenderer>();
             if (currentDestinationLineRenderer) {
                 currentDestinationLineRenderer.SetPosition(0, currentDestination.transform.position);
                 currentDestinationLineRenderer.SetPosition(1, transform.position);
@@ -46,53 +87,35 @@ namespace Control {
             waypoints.Remove(next);
         }
 
-        public void ClearWaypoints() {
+        private void ClearWaypoints() {
             Destroy(currentDestination);
             waypoints.ForEach(Destroy);
             waypoints.Clear();
         }
 
-        /// <summary>
-        /// Adds a waypoint and renders a line to the previous wapoint
-        /// </summary>
-        public void AddWaypoint(ClickLocation clickLocation) {
-            var markerWrapper = new GameObject("Waypoint");
-            markerWrapper.transform.position = clickLocation.Location;
-            var markerLocation = clickLocation.Location;
-            markerLocation.y += InactiveWaypointMarkerPrefab.transform.localScale.y / 2;
-            var marker = Instantiate(
-                InactiveWaypointMarkerPrefab,
-                markerLocation,
-                InactiveWaypointMarkerPrefab.transform.rotation
-            );
-            marker.transform.SetParent(markerWrapper.transform);
-            waypoints.Add(markerWrapper);
-            // Connect waypoint to previous waypoint
-            var lineRenderer = marker.GetComponent<LineRenderer>();
-//            lineRenderer.startColor = WaypointLineColor;
-//            lineRenderer.endColor = WaypointLineColor;
-//            lineRenderer.startWidth = WaypointLineWidth;
-//            lineRenderer.endWidth = WaypointLineWidth;
-
-            var previousWaypoint = waypoints.Count < 2 ? currentDestination : waypoints[waypoints.Count - 2];
-            if (previousWaypoint == null) return;
-            lineRenderer.SetPosition(0, waypoints[waypoints.Count - 1].transform.position);
-            lineRenderer.SetPosition(1, previousWaypoint.transform.position);
-            currentDestinationLineRenderer.enabled = true;
-        }
-
-        public void UpdateCurrentWaypointLine() {
+        private void UpdateCurrentWaypointLine() {
             if (currentDestinationLineRenderer != null && currentDestinationLineRenderer.enabled) {
                 currentDestinationLineRenderer.SetPosition(1, transform.position);
             }
         }
 
-        public void ToggleWaypointRenderer(bool value) {
-            waypoints.ForEach(o => {
-                o.SetActive(value);
-                var line = o.GetComponentInChildren<LineRenderer>();
-                line.enabled = value;
-            });
+        private void Start() {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update() {
+            if (Animator != null) Animator.SetFloat("Speed", agent.velocity.magnitude);
+            UpdateCurrentWaypointLine();
+            var arrived = agent.hasPath && agent.remainingDistance <= agent.stoppingDistance;
+            if (!agent.hasPath && waypoints.Count > 0) {
+                GoToNextWaypoint();
+                return;
+            }
+            if (!arrived) return;
+
+            DestroyCurrentWaypoint();
+            agent.ResetPath();
+            GoToNextWaypoint();
         }
     }
 }
