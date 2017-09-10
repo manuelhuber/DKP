febdc8e16b98b129828ec16f682e028d0f936f7d
Manuel Huber <nelo112@web.de>
2017-09-10 11:51:04 +0200

    Made camer ortographic

--- Assets/Scripts/MyCamera/ZoomWithMouseWheel.cs
+++ Assets/Scripts/MyCamera/ZoomWithMouseWheel.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 namespace MyCamera {
     public class ZoomWithMouseWheel : MonoBehaviour {
         public float ZoomMultiplier;
-        public float FoVMax = 40;
-        public float FoVMin = 10;
+        public float ZoomMax = 40;
+        public float ZoomMin = 10;
 
         private void Update() {
             var scrollValue = Input.GetAxis("Mouse ScrollWheel");
             if (!(Math.Abs(scrollValue) > 0.01)) return;
-            var fov = Camera.main.fieldOfView - scrollValue * ZoomMultiplier;
-            fov = Mathf.Max(fov, FoVMin);
-            fov = Mathf.Min(fov, FoVMax);
-            Camera.main.fieldOfView = fov;
+            var size = Camera.main.orthographicSize - scrollValue * ZoomMultiplier;
+            size = Mathf.Max(size, ZoomMin);
+            size = Mathf.Min(size, ZoomMax);
+            Camera.main.orthographicSize = size;
         }
     }
 }
