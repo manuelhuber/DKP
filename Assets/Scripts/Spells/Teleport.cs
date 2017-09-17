e26c6b3b7ddd93e8e7c052412ee6bcbda6d56bbc
Manuel Huber <nelo112@web.de>
2017-09-17 02:14:53 +0200

    made shadowblink animation nice

--- Assets/Scripts/Spells/Teleport.cs
+++ Assets/Scripts/Spells/Teleport.cs
@@ -1,4 +1,5 @@
 ﻿using Control;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.AI;
 using Util;
@@ -8,12 +9,15 @@ namespace Spells {
     public class Teleport : Ability {
         public float Distance;
         public GameObject MarkerPrefab;
+        public GameObject LeavePrefab;
 
         private GameObject caster;
         private GameObject marker;
+        private GameObject leaveMarker;
 
         public override bool OnActivation(GameObject c) {
             caster = c;
+            leaveMarker = Instantiate(LeavePrefab, caster.transform);
             marker = Instantiate(MarkerPrefab);
             marker.transform.SetParent(caster.transform, false);
             marker.transform.position = WarpLocation();
@@ -32,8 +36,11 @@ namespace Spells {
             if (caster == null) return false;
             var agent = caster.GetComponent<NavMeshAgent>();
             if (agent == null) return false;
+            leaveMarker.transform.SetParent(null, true);
             agent.Warp(WarpLocation());
             Destroy(marker);
+            leaveMarker.GetComponent<Animator>().SetTrigger("Die");
+            Destroy(leaveMarker, 0.5f);
 
             var waypoints = caster.GetComponent<WaypointHandler>();
             if (waypoints != null) {
@@ -44,6 +51,7 @@ namespace Spells {
 
         public override void OnCancel() {
             Destroy(marker);
+            Destroy(leaveMarker);
             caster = null;
         }
 
