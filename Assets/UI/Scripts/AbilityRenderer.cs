4fc59a77581dd4678354904d4cc6f8d09feb652c
Manuel Huber <nelo112@web.de>
2017-09-23 15:02:01 +0200

    Moved UI stuff to own folder

---
+++ Assets/UI/Scripts/AbilityRenderer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Control;
+using UnityEngine;
+using UnityEngine.UI;
+using Util;
+
+namespace UI {
+    public class AbilityRenderer : MonoBehaviour {
+        public GameObject AbilityPrefab;
+
+        private readonly List<GameObject> renderedAbilities = new List<GameObject>();
+        private List<ActiveAbility> abilities;
+        private Transform folder;
+
+        public void DisplayAbilities(List<ActiveAbility> abs) {
+            renderedAbilities.ForEach(Destroy);
+            renderedAbilities.Clear();
+            abilities = abs;
+            if (abs == null) return;
+            abs.ForEach(aa => DisplayAbility(aa.Ability));
+        }
+
+        public void DisplayAbility(Ability ab) {
+            var ability = Instantiate(AbilityPrefab, folder);
+            ability.GetComponentInChildren<Image>().sprite = ab.Icon;
+            var hotkey = UnityUtil.FindComponentInChildrenWithTag<Text>(ability, "HotkeyText");
+            hotkey.text = ab.Hotkey.ToString();
+
+            var rect = ability.GetComponent<RectTransform>();
+            var offsetX = renderedAbilities.Count * (rect.rect.height + 10);
+            UnityUtil.SetAnchoredPosition(rect, offsetX, 0);
+            renderedAbilities.Add(ability);
+        }
+
+        private void Start() {
+            folder = GameObject.FindWithTag("AbilityFolder").transform;
+        }
+
+
+        private void Update() {
+            var i = 0;
+            if (abilities == null) return;
+            abilities.ForEach(ability => {
+                var cooldown = Math.Round(ability.RemainingCooldown, 1, MidpointRounding.AwayFromZero);
+                UnityUtil.FindComponentInChildrenWithTag<Text>(renderedAbilities[i], "CooldownText").text =
+                    cooldown > 0 ? cooldown.ToString("n1") : "";
+                i++;
+            });
+        }
+    }
+}
---
+++ Assets/UI/Scripts/AvatarRenderer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Util;
+
+namespace UI {
+    public class AvatarRenderer : MonoBehaviour {
+        public float SpaceBetweenAvatars = 10;
+
+        private int avatarCount;
+        private Transform folder;
+
+        public void InitializeAvater(GameObject prefab, out Slider healthbar, string avaterName) {
+            var avatar = Instantiate(prefab);
+            avatar.transform.SetParent(folder);
+            var rectTransform = UiUtil.RectTransfromAnchorTopLeft(avatar.GetComponent<RectTransform>());
+
+            // calculate position based on number of avatars
+            var offsetTop = (rectTransform.rect.height + SpaceBetweenAvatars) * -avatarCount;
+            UnityUtil.SetAnchoredPosition(rectTransform, rectTransform.anchoredPosition.x, offsetTop);
+
+            // set the out-parameter
+            healthbar = avatar.GetComponentInChildren<Slider>();
+
+            // Add name
+            avatar.GetComponentInChildren<Text>().text = avaterName;
+
+            avatarCount++;
+        }
+
+        private void Awake() {
+            folder = GameObject.FindWithTag("AvatarFolder").transform;
+        }
+    }
+}
