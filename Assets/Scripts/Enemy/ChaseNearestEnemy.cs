e3a7d3b729b85b56f5225fb18678a9779034ae89
Manuel Huber <nelo112@web.de>
2017-09-23 23:58:06 +0200

    added new enemy

---
+++ Assets/Scripts/Enemy/ChaseNearestEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Runtime.InteropServices;
+using Damage;
+using Damage.Common;
+using Raid;
+using UnityEngine;
+using UnityEngine.AI;
+using Util;
+
+namespace Enemy {
+    public class ChaseNearestEnemy : MonoBehaviour {
+        [Tooltip("The time in s between checking for nearer enemies ")] public float ReevaluateInterval = 2;
+        private NavMeshAgent agent;
+        private Attack attack;
+        private Team team;
+        private float nextEnemyCheck;
+
+        private void Update() {
+            if (NotActive()) return;
+            if (nextEnemyCheck <= Time.time) {
+                attack.SetTarget(TargetManager.GetEnemies(team.TeamId)
+                    .Aggregate(null, PositionUtil.FindNearest(gameObject.transform.position))
+                    .GetComponent<Damageable>());
+
+                nextEnemyCheck = Time.time + ReevaluateInterval;
+            }
+            agent.isStopped = attack.InRange;
+            var t = attack.GetTarget();
+            if (t != null) agent.SetDestination(t.gameObject.transform.position);
+        }
+
+        private bool NotActive() {
+            return agent == null || attack == null || !agent.enabled || !attack.enabled;
+        }
+
+        private void Awake() {
+            agent = GetComponent<NavMeshAgent>();
+            attack = GetComponent<Attack>();
+            team = GetComponent<Team>();
+        }
+    }
+}
