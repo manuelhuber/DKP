3ee017dfbdf2a096fa466c16331b0c2c0b8f309f
Manuel Huber <nelo112@web.de>
2017-09-30 23:27:10 +0200

    added heal to smokebomb

---
+++ Assets/Abilities/Scripts/SmokeBomb.cs
@@ -0,0 +1,21 @@
+﻿using Control;
+using Damage;
+using UnityEngine;
+
+namespace Abilities.Scripts {
+    public class SmokeBomb : Ability {
+        public float Duration;
+        public int HealingAmount;
+
+        public GameObject EffectPrefab;
+        public float EffectDuration;
+
+        public override bool OnActivation(GameObject caster) {
+            var damageable = caster.GetComponent<Damageable>();
+            damageable.MakeUntargetableFor(Duration);
+            damageable.ModifyHitpoints(HealingAmount);
+            Destroy(Instantiate(EffectPrefab, caster.transform.position, Quaternion.identity), EffectDuration);
+            return true;
+        }
+    }
+}
