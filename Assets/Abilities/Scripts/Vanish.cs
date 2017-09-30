399dbe2164c83197bcb3c5d709473b4ebafdc882
Manuel Huber <nelo112@web.de>
2017-09-30 12:54:15 +0200

    added vanish ability

---
+++ Assets/Abilities/Scripts/Vanish.cs
@@ -0,0 +1,14 @@
+﻿using Control;
+using Damage;
+using UnityEngine;
+
+namespace Abilities.Scripts {
+    public class Vanish : Ability {
+        public float Duration;
+
+        public override bool OnActivation(GameObject caster) {
+            caster.GetComponent<Damageable>().MakeUntargetableFor(Duration);
+            return true;
+        }
+    }
+}
